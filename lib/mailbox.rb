73bd657b154609bb7deead334e86403afb624b49
adrianke77 <adrianke77@gmail.com>
2017-01-30 17:15:10 +0800

    done

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,13 +1,23 @@
 class MailBox
   def initialize(letters, packages)
+    @letters = letters
+    @packages = packages
   end
 
+  attr_reader :letters
+
+  attr_reader :packages
+
   def add_letter
+    @letters += 1
   end
 
   def add_package
+    @packages += 1
   end
 
   def clear
+    @packages = 0
+    @letters = 0
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,17 @@
 class Tree
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
+  attr_reader :type
+  attr_writer :height
+  attr_reader :height
+  attr_writer :age
+  attr_reader :age
+
   def grow_tree(water_amount)
+    @height += water_amount/10
   end
 end
